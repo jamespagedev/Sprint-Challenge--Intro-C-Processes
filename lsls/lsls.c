f44f846b1bad593ceb120e87dd40a12896ba025c
jamespagedev <jamespagedev@outlook.com>
2019-04-26 13:41:50 -0700

    stretch - closer

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -34,11 +34,11 @@ void print_dir_details(struct stat item_stat, char *item_name, int dir_level)
   }
 }
 
-void print_dirs(char *dir, int dir_level)
+void print_dirs(char *orig_dir, char *curr_dir, int dir_level)
 {
-  printf("Directory = \"%s\"\n", dir);
+  printf("Directory = \"%s\"\n", curr_dir);
   // Open directory
-  DIR *dir_details = opendir(dir);
+  DIR *dir_details = opendir(curr_dir);
   struct dirent *dir_item;
 
   // Print items in directories
@@ -48,8 +48,8 @@ void print_dirs(char *dir, int dir_level)
     char *item_name = dir_item->d_name;
 
     // required dir\filename for stat()
-    char dir_with_item[strlen(dir) + strlen(item_name) + 1];
-    strcpy(dir_with_item, dir);
+    char dir_with_item[strlen(orig_dir) + strlen(item_name) + 1];
+    strcpy(dir_with_item, orig_dir);
     strcat(dir_with_item, "\\");
     strcat(dir_with_item, item_name);
 
@@ -57,12 +57,19 @@ void print_dirs(char *dir, int dir_level)
     struct stat item_stat;
     stat(dir_with_item, &item_stat);
     print_dir_details(item_stat, item_name, dir_level);
-    //prints out weird, can't figure this "stretch" recursion out
-    // if (dir_level == 1)
-    // {
-    //   printf("\n");
-    //   print_dirs(item_name, 2); // 2 is for child directory
-    // }
+    // prints out weird, can't figure this "stretch" recursion out
+    if (dir_level == 1)
+    {
+      printf("\n");
+      if (S_ISREG(item_stat.st_mode))
+      {
+        print_dirs(orig_dir, item_name, 2); // 2 is for child directory
+      }
+      else if (S_ISDIR(item_stat.st_mode)) // item is dir
+      {
+        print_dirs(orig_dir, dir_with_item, 2); // 2 is for child directory
+      }
+    }
   }
 
   // Close directory
@@ -77,25 +84,28 @@ int main(int argc, char **argv)
   // pre-condition: all args AFTER argv[1] will be ignored
   // Note: argc is at least 1 for the current file name typed from user.
   // Parse command line
-  char *dir;
+  char *orig_dir;
+  char *curr_dir;
   int dir_level = 1;
 
   // Check Directory is Valid
   if (argc - 1 > 0) // dir was given
   {
-    dir = argv[1];
-    DIR *check_dir = opendir(dir);
+    orig_dir = argv[1];
+    curr_dir = argv[1];
+    DIR *check_dir = opendir(curr_dir);
     if (ENOENT == errno)
     {
-      printf("Directory \"%s\" does not exist\n", dir);
+      printf("Directory \"%s\" does not exist\n", curr_dir);
       return 1;
     }
     closedir(check_dir);
   }
   else // no dir given
   {
-    dir = ".";
-    DIR *check_dir = opendir(dir);
+    orig_dir = ".";
+    curr_dir = ".";
+    DIR *check_dir = opendir(curr_dir);
     if (ENOENT == errno)
     {
       printf("Your environment is not setup correclty to run this file\n");
@@ -105,7 +115,7 @@ int main(int argc, char **argv)
   }
 
   // Repeatly read and print entries
-  print_dirs(dir, dir_level);
+  print_dirs(orig_dir, curr_dir, dir_level);
 
   return 0;
 }
